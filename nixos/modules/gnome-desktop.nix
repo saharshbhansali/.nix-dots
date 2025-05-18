22be5809e4b27a4e3a495d1914f553b202394c26
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-05-18 14:37:48 +0000

    update: GNOME desktop - config to setup xdg-portal, enable DE, and add a few gnome-shell-extensions
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/gnome-desktop.nix
+++ nixos/modules/gnome-desktop.nix
@@ -2,7 +2,54 @@
 
 {
 
+  # imports = [
+  #   ./gdm.nix
+  # ];
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+  xdg.portal.config.common.default = "*";
+
+  # Enable GNOME
+  services.xserver.desktopManager.gnome.enable = true;
+
   # dconf editor for GNOME settings
   programs.dconf.enable = true;
 
+  # GNOME Tweaks
+  environment.systemPackages = with pkgs; [
+    # gsettings
+    nwg-look
+    gnome-shell
+    gnome-tweaks
+    gnomeExtensions.apps
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.dash-to-panel
+    gnomeExtensions.user-themes
+    gnomeExtensions.system-monitor
+    gnomeExtensions.pano
+    gnomeExtensions.window-list
+    gnomeExtensions.workspace-indicator
+    gnomeExtensions.unmess
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.vitals
+    gnomeExtensions.cmud
+    gnomeExtensions.forge
+    gnomeExtensions.focus
+    gnomeExtensions.freon
+    gnomeExtensions.gmeet
+    gnomeExtensions.gsnap
+    gnomeExtensions.gtile
+    gnomeExtensions.gdeej
+    gnomeExtensions.sermon
+    gnomeExtensions.docker
+    gnomeExtensions.copier
+    gnomeExtensions.clipqr
+    gnomeExtensions.yakuake
+    gnomeExtensions.zilence
+    gnomeExtensions.wallhub
+    gnomeExtensions.arcmenu
+  ];
 }
