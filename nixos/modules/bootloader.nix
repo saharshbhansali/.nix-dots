75c7fb1913b216bffc31a00a00eeec0d54de3155
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-05-18 15:21:34 +0000

    feat: add grub theme and reference for using custom (unpackaged) theme
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 {
 
+  environment.systemPackages = with pkgs; [
+    catppuccin-grub
+  ];
+
   ## Configure bootloader - GRUB
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader = {
@@ -9,12 +13,23 @@
       efiSysMountPoint = "/boot/efi";
     };
     grub = {
-       enable = lib.mkDefault true;
-       efiSupport = true;
-       useOSProber = true;
-       #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-       devices = ["nodev"];
-       theme = pkgs.where-is-my-sddm-theme;
+      enable = lib.mkDefault true;
+      efiSupport = true;
+      useOSProber = true;
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      devices = ["nodev"];
+      theme = pkgs.catppuccin-grub;
+      # theme = pkgs.stdenv.mkDerivation {
+      #   pname = "distro-grub-themes";
+      #   version = "3.1";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "AdisonCavani";
+      #     repo = "distro-grub-themes";
+      #     rev = "v3.1";
+      #     hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+      #   };
+      #   installPhase = "cp -r customize/nixos $out";
+      # };
     };
   };
 
