097f21e09eefde3f3ca7687422e483a7634952ec
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-05-18 15:21:53 +0000

    fix: gdm enable wayland syntax error
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/gdm.nix
+++ nixos/modules/gdm.nix
@@ -5,6 +5,6 @@
   # Enable GDM
   services.xserver.enable =  true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
 
 }
