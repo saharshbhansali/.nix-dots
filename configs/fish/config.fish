c05480112151a22f0ac08b3eb076ba0b13d12372
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-06-15 15:58:05 +0530

    feat: add fish config
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

---
+++ configs/fish/config.fish
@@ -0,0 +1,49 @@
+eval $(starship init fish)
+
+# # Install plugins if not already installed
+# set -l plugins \
+#     IlanCosman/tide@v5 \
+#     jorgebucaran/nvm.fish \
+#     jorgebucaran/autopair.fish \
+#     PatrickF1/fzf.fish
+#
+# for plugin in $plugins
+#     fisher list | grep -q (string split @ $plugin)[1]; or fisher install $plugin
+# end
+
+# Environment variables
+if set -q SSH_CONNECTION
+    set -gx EDITOR vim
+else
+    set -gx EDITOR nvim
+end
+set -gx VISUAL emacs
+set -gx VAGRANT_DEFAULT_PROVIDER virtualbox
+
+# Fzf 
+fzf --fish | source
+
+# Zoxide
+zoxide init fish | source
+
+# Atuin
+atuin init fish | source
+
+# Carapace completions
+set -gx CARAPACE_BRIDGES "zsh,fish,bash,inshellisense"
+carapace _carapace | source
+
+# Pay-respects
+pay-respects init fish | source
+
+# Keybindings (example)
+bind \cp up-or-search
+bind \cn down-or-search
+
+# Load FZF keybindings if needed
+fzf_key_bindings
+
+# Load custom aliases and autorun scripts if they exist
+for file in ~/.config/fish/conf.d/*.fish
+    test -f $file; and source $file
+end
