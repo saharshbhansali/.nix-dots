c43e6984b2fcd65ade8fb25ee2600582dff3b807
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-06-29 23:03:13 +0530

    update: clean-up dev utils and tools
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -6,7 +6,7 @@
 
   environment.systemPackages = with pkgs; [
 
-    # Nix utils
+    ## Nix utils
     cachix
     steam-run
     appimage-run
@@ -15,13 +15,13 @@
     nh
     nix-output-monitor
 
-    # Hypr utils
+    ## Hypr utils
     hyprlock
     hypridle
     hyprcursor
     numlockx
 
-    # Terminal essentials
+    ## Terminal essentials
     fish
     zsh
     git
@@ -29,17 +29,18 @@
     # neovim
     # vimPlugins.nvim-treesitter.withPlugins
 
-    # Terminal software
+    ## Terminal software
     kitty
     yazi
 
     ## Terminal utils
+    gunzip
 
-    # Networking utils
+    ## Networking utils
     wget
     curl
 
-    # Info utils
+    ## Info utils
     # most
     less
     more
@@ -51,8 +52,9 @@
     pciutils
     procs
     dust
+    strace
 
-    # Terminal enhancers
+    ## Terminal enhancers
     rip2
     eza
     bat
@@ -62,7 +64,7 @@
     jq
     sd
 
-    # Terminal customization
+    ## Terminal customization
     zoxide
     pay-respects
     starship
@@ -72,12 +74,12 @@
     bottom
     diff-so-fancy
 
-    # Fetch utils
+    ## Fetch utils
     pfetch
     fastfetch
     neofetch
 
-    # Keyboard and Clipboard utils
+    ## Keyboard and Clipboard utils
     wl-clipboard
     xclip
     xorg.xcbutil
@@ -85,83 +87,84 @@
     xorg.setxkbmap
     xorg.xev
 
-    # Gesture utils
+    ## Gesture utils
     libinput
     libinput-gestures
     wmctrl
     xdotool
 
-    # Disk utils
+    ## Disk utils
     parted
     gparted
     efibootmgr
 
-    # Backup utils
+    ## Backup utils
     vorta
     borgbackup
     pika-backup
 
-    # Media utils
+    ## Media utils
     ffmpeg
     pamixer
 
-    # Development utils
-    devenv
-    direnv
+    ## Development utils
+    devenv                          # development environment manager
+    direnv                          # environment variable manager
+    bintools                        # tools for manipulating binaries
     gcc                             # gcc compiler
+    gdb                             # gdb debugger
     clang                           # clang compiler
     gnumake                         # make build tool
     cmake                           # cmake build system
+    python-launcher                 # python version manager
+    pipx                            # install python packages globally
     python313                       # python 3.13 interpreter
     python313Packages.pip           # python 3.13 package manager
     python313Packages.virtualenv    # python 3.13 virtual environment
     python312                       # python 3.12 interpreter
     python312Packages.pip           # python 3.12 package manager
     python312Packages.virtualenv    # python 3.12 virtual environment
-    python-launcher                 # python version manager
-    pipx                            # install python packages globally
-    strace                          # for tracing system calls
-    gdb                             # for debugging
     asdf-vm                         # asdf version manager
     volta                           # node version manager
     go                              # go programming language
     rustc                           # rust compiler
-    rustup                          # rust package manager
+    rustup                          # rust toolchain installer
+    cargo                           # rust package manager
 
-    # KDE Wallet utils
+    ## KDE Wallet utils
     # kwalletcli
     # kdepackages.kwallet
     kdePackages.kwalletmanager
 
-    # GTK utils
+    ## GTK utils
     gtk2
     gtk3
     gtk4
 
-    # BitWarden
+    ## BitWarden
     bitwarden-cli
     bitwarden-desktop
     bitwarden-menu
 
-    # Office software
+    ## Office software
     onlyoffice-bin
 
-    # File Management software
+    ## File Management software
     kdePackages.filelight
     xfce.thunar
     xfce.thunar-archive-plugin
 
-    # # VM Software
+    # ## VM Software
     # vmware-workstation
 
-    # Cursors and icons
+    ## Cursors and icons
     catppuccin-cursors.mochaRed
     catppuccin-cursors.mochaMauve
     catppuccin-cursors.mochaLavender
     rose-pine-cursor
     # rose-pine-hyprcursor
 
-    # Fonts
+    ## Fonts
     nerd-fonts.jetbrains-mono
     nerd-fonts.droid-sans-mono
     nerd-fonts.fira-code
