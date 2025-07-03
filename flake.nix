b5b1a0f3b77720ed3c8b367ae97e8c2c8a2c9300
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-07-03 16:39:16 +0530

    feat (wip): use nvf
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- flake.nix
+++ flake.nix
@@ -19,6 +19,24 @@
       # inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Optional, if you intend to follow nvf's obsidian-nvim input
+    # you must also add it as a flake input.
+    # obsidian-nvim = {
+    #   url = "github:epwalsh/obsidian.nvim";
+    # };
+
+    # Required, nvf works best and only directly supports flakes
+    nvf = {
+      url = "github:notashelf/nvf";
+      # You can override the input nixpkgs to follow your system's
+      # instance of nixpkgs. This is safe to do as nvf does not depend
+      # on a binary cache.
+      inputs.nixpkgs.follows = "nixpkgs";
+      # Optionally, you can also override individual plugins
+      # for example:
+      # inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
