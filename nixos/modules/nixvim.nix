2c5562ca437756f1284bd2195db7a0d9ce484918
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-07-03 19:45:35 +0530

    update: use programs.nixvim instead of raw nvim
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/nixvim.nix
+++ nixos/modules/nixvim.nix
@@ -1,70 +1,77 @@
 { config, pkgs, lib, inputs, ... }:
 
-let
-  lazySpec = ''
-    require("lazy").setup({
-      spec = {
-        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-        { "nvim-telescope/telescope-fzf-native.nvim", enabled = true },
-        { "williamboman/mason.nvim", enabled = false },
-        { "williamboman/mason-lspconfig.nvim", enabled = false },
-        { import = "plugins" },
-        { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
-      },
-      defaults = { lazy = true },
-      install = { missing = false },
-      change_detection = { enabled = false },
-      performance = {
-        cache = { enabled = true },
-        reset_packpath = false,
-        rtp = { reset = false },
-      }
-    })
-  '';
+{
+  imports = [ inputs.nixvim.nixosModules.nixvim ];
 
-  treesitterParsers = pkgs.symlinkJoin {
-    name = "nvim-treesitter-parsers";
-    paths = pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
-      lua c cpp python rust go nix bash zsh fish
-      javascript typescript html css json
-      java zig markdown
-    ]).dependencies;
+  programs.nixvim = {
+    enable = true;
+
+    clipboard.register = "unnamedplus";
+    clipboard.providers.wl-copy.enable = true;
+    clipboard.providers.xclip.enable = true;
+
+    colorschemes.catppuccin.enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+
+    luaLoader.enable = true;
+
+    # plugins = {
+    #   lazy.enable = true;
+    #
+    #   lsp = {
+    #     enable = true;
+    #     servers = {
+    #       lua_ls.enable = true;
+    #       rust_analyzer = {
+    #         enable = true;
+    #         installCargo = true;
+    #         installRustc = true;
+    #       };
+    #     };
+    #   };
+    #
+    #   treesitter.enable = true;
+    #   which-key.enable = true;
+    #   lualine.enable = true;
+    #   yanky.enable = true;
+    #   harpoon.enable = true;
+    #   telescope.enable = true;
+    #   mini.enable = true;
+    #   web-devicons.enable = true;
+    # };
   };
-in {
-  imports = [inputs.nixvim.nixosModules.default];
+
+
   environment.systemPackages = with pkgs; [
-    neovim
-    # Language servers
-    lua-language-server
-    stylua
-    python3Packages.python-lsp-server
-    rust-analyzer
-    gopls
-    # jdtls
-    clang-tools
-    zls
-    nixd
-    bash-language-server
-    typescript-language-server
-    tailwindcss-language-server
-    marksman
-    # Dev tools
-    ripgrep
-    fzf
+      # neovim
+      # Language servers
+      lua-language-server
+      stylua
+      python3Packages.python-lsp-server
+      rust-analyzer
+      gopls
+      # jdtls
+      clang-tools
+      zls
+      nixd
+      bash-language-server
+      typescript-language-server
+      tailwindcss-language-server
+      marksman
+      # Dev tools
+      ripgrep
+      fzf
   ];
 
-  # # Neovim init.lua
-  # environment.etc."nvim/init.lua".text = lazySpec;
-
-  # Optional: mount your own custom Lua config directory
+  # Mount custom config directory
   environment.etc."lazynvim".source = ../../configs/nvim;
 
   # Create a symlink to ~/.config/nvim → /etc/lazyvim
   systemd.user.tmpfiles.rules = [
-    "L+ /root/.config/nvim - - - - /etc/lazyvim"
+      "L+ /root/.config/nvim - - - - /etc/lazyvim"
+      "L+ /etc/nvim - - - - /etc/lazyvim"
   ];
-
-  # Treesitter parser binaries
-  # environment.etc."nvim/parser".source = "${treesitterParsers}/parser";
 
 }
