04948538265f8371d274337352eff0b3dd9b34ec
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-07-03 16:53:50 +0530

    feat: working lazyvim config
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/nixvim.nix
+++ nixos/modules/nixvim.nix
@@ -1,20 +1,56 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  # assuming nvf is in your inputs and inputs is in the argset
-  # see example below
-  imports = [ inputs.nvf.nixosModules.default ];
+let 
+  vimPkgs = pkgs.vimPlugins;
+  lazyPlugins = with vimPkgs; [
+    lazy-nvim
+    nvim-treesitter
+  ];
 
-  programs.nvf = {
-    enable = true;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
-    settings = {
-      vim.viAlias = false;
-      vim.vimAlias = true;
-      vim.lsp = {
-        enable = true;
-      };
-    };
+  lazySpec = ''
+    require("lazy").setup({
+      defaults = { lazy = true },
+      install = { missing = false },
+      change_detection = { enabled = false },
+      performance = { packpath = { reset = false }, rtp = { reset = false } },
+      spec = {
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        { "nvim-telescope/telescope-fzf-native.nvim", enabled = true },
+        { "williamboman/mason-lspconfig.nvim", enabled = false },
+        { "williamboman/mason.nvim", enabled = false },
+        -- Your extra plugins here
+      },
+    })
+  '';
+
+  treesitterParsers = pkgs.symlinkJoin {
+    name = "nvim-treesitter-parsers";
+    paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
   };
+
+in {
+  environment.systemPackages = with pkgs; [
+    neovim
+    lua-language-server
+    stylua
+    ripgrep
+    python3Packages.python-lsp-server
+    rust-analyzer
+    gopls
+    bash-language-server
+    typescript-language-server
+    tailwindcss-language-server
+    zig
+    zls
+    # add more if needed
+  ];
+
+  # This mounts lazy.nvim and any other plugins you want preloaded
+  environment.etc."nvim/init.lua".text = lazySpec;
+
+  # This adds Treesitter parser .so files to XDG_CONFIG_HOME/nvim/parser
+  # xdg.configFile."nvim/parser".source = "${treesitterParsers}/parser";
+
+  # Optional: mount your `lua/` config
+  # xdg.configFile."nvim/lua".source = ./lua;
 }
