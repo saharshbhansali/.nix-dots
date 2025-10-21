dcecc4a99688721f3411b5e7dca429c71d81444d
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-21 23:48:27 +0530

    feat: add flathub-beta remote
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- home-manager/modules/hm-flatpaks.nix
+++ home-manager/modules/hm-flatpaks.nix
@@ -4,6 +4,10 @@
 
   imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
 
+  services.flatpak.remotes = [{
+    name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+  }];
+
   services.flatpak.packages = [
     { appId = "com.stremio.Stremio"; origin = "flathub-beta"; }
     # "com.obsproject.Studio"
