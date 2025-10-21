c02c893b0a3874cc0b41ae6d8a730e4329943f3e
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-21 22:11:48 +0530

    feat: inherit read-only packages to prevent specialArgs.pkgs warning
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- flake.nix
+++ flake.nix
@@ -65,7 +65,13 @@
         inherit system;
         specialArgs = { inherit inputs pkgs; };
         modules = [
+          # prevents warning about specialArgs.pkgs
+          nixpkgs.nixosModules.readOnlyPkgs
+
+          # hardware configuration
           ./hardware-configuration.nix
+
+          # User configuration
           ./nixos/hosts/default.nix
 
           # Home Manager as NixOS module
