e02093f1a1d3d7e76c0099036752114232933823
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-21 23:50:38 +0530

    feat: auto-update flatpaks
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- home-manager/modules/hm-flatpaks.nix
+++ home-manager/modules/hm-flatpaks.nix
@@ -4,10 +4,19 @@
 
   imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
 
-  services.flatpak.remotes = [{
-    name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+  # Append additional remotes to default remote 'flathub'
+  services.flatpak.remotes = lib.mkOptionDefault [{
+    name = "flathub-beta";
+    location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
   }];
 
+  # Automatically update flatpaks
+  services.flatpak.update.auto = {
+    enable = true;
+    onCalendar = "weekly"; # Default value
+  };
+
+  # List of flatpaks to install
   services.flatpak.packages = [
     { appId = "com.stremio.Stremio"; origin = "flathub-beta"; }
     # "com.obsproject.Studio"
