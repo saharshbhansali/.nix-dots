b9284b9e385a6dd51f32a37705304b5f68c15583
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-26 13:29:15 +0530

    feat: nvidia optimus prime offload and (specialisation) sync support
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,9 +4,9 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
@@ -52,15 +52,37 @@
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
       prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
         amdgpuBusId = "PCI:5:0:0";     # AMD iGPU
         nvidiaBusId = "PCI:1:0:0";     # NVIDIA dGPU
       };
       # Use the open kernel module (required for driver >= 560 on Turing+ GPUs)
       open = true;
+    };
+  };
+
+  specialisation = {
+    gaming-time.configuration = {
+      hardware.nvidia = {
+        prime.sync.enable = lib.mkForce true;
+        prime.offload = {
+          enable = lib.mkForce false;
+          enableOffloadCmd = lib.mkForce false;
+        };
+      };
     };
   };
 
