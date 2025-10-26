b7aea7ca85c5780484c03aa7ce7e01c59f1882d5
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-26 06:59:36 +0530

    fix (wip): switch to 6.12-LTS kernel packages
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/hosts/default.nix
+++ nixos/hosts/default.nix
@@ -85,6 +85,7 @@
   hardware.usb-modeswitch.enable = true;
   hardware.enableAllFirmware = true;
   # Optional: Use latest kernel for better Realtek driver support
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
 
 }
