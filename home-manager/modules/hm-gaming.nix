405a5c3fce1353dd2c473a9a4f4163ff949b7f10
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-26 14:00:54 +0530

    feat: add system-level and home-manager gaming config
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

---
+++ home-manager/modules/hm-gaming.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+
+  ## GPU drivers
+  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+
+  ## Enable gamemode
+  programs.gamemode.enable = true;
+
+  ## Steam
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin];
+  };
+
+  ## Gaming packages
+  home.packages = with pkgs; [
+    # HUD for system performance
+    mangohud
+
+    # Proton & Wine
+    protonup-ng
+    wine
+    wine64
+
+    # Launchers
+    lutris
+    heroic
+    bottles
+
+    # Extras
+    playonlinux
+    protontricks
+    protonplus
+    protonup-qt
+    winetricks
+  ];
+
+  home.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+
+}
+
