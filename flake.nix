62a12da5c74e04f0b960dcddf06cb0d92a18d6b6
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-27 18:24:22 +0530

    feat: spicetify-nix flake input
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- flake.nix
+++ flake.nix
@@ -57,6 +57,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs = { self, nixpkgs, home-manager, nur, nixvim, ... } @ inputs:
