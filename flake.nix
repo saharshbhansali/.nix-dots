995271e0c44d4d9b665f7ca3ea84ba79600080fc
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-27 18:16:53 +0530

    refactor: remove nvf flake input
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- flake.nix
+++ flake.nix
@@ -26,27 +26,27 @@
       # inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Optional, if you intend to follow nvf's obsidian-nvim input
-    # you must also add it as a flake input.
-    # obsidian-nvim = {
-    #   url = "github:epwalsh/obsidian.nvim";
-    # };
-
-    # Required, nvf works best and only directly supports flakes
-    nvf = {
-      url = "github:notashelf/nvf";
-      # You can override the input nixpkgs to follow your system's
-      # instance of nixpkgs. This is safe to do as nvf does not depend
-      # on a binary cache.
-      inputs.nixpkgs.follows = "nixpkgs";
-      # Optionally, you can also override individual plugins
-      # for example:
-      # inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
-    };
-
     nixCatsNvim = {
       url = "path:./nixos/modules/nvim";
     };
+
+    # # Required, nvf works best and only directly supports flakes
+    # nvf = {
+    #   url = "github:notashelf/nvf";
+    #   # You can override the input nixpkgs to follow your system's
+    #   # instance of nixpkgs. This is safe to do as nvf does not depend
+    #   # on a binary cache.
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   # Optionally, you can also override individual plugins
+    #   # for example:
+    #   # inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
+    # };
+    #
+    # # Optional, if you intend to follow nvf's obsidian-nvim input
+    # # you must also add it as a flake input.
+    # # obsidian-nvim = {
+    # #   url = "github:epwalsh/obsidian.nvim";
+    # # };
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
