34df1f6662ad21fddb62f8b3c5b37ba1f8aafb60
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-10-29 12:53:35 +0530

    feat (wip): use nvidia graphics settings from nix-wiki: https://nixos.wiki/wiki/Nvidia
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -2,6 +2,13 @@
 
 {
 
+  ## GPU drivers
+  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+
+  ## Disable the integrated GPU by blacklisting the module
+  # # AMD
+  # boot.kernelParams = [ "module_blacklist=amdgpu" ];
+
   hardware = {
     graphics = {
       enable = true;
@@ -9,8 +16,34 @@
     };
 
     nvidia = {
+      # Modesetting is required.
       modesetting.enable = true;
-      powerManagement.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of 
+      # supported GPUs is at: 
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Only available from driver 515.43.04+
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
       prime = {
         offload = {
           enable = true;
@@ -19,8 +52,6 @@
         amdgpuBusId = "PCI:5:0:0";     # AMD iGPU
         nvidiaBusId = "PCI:1:0:0";     # NVIDIA dGPU
       };
-      # Use the open kernel module (required for driver >= 560 on Turing+ GPUs)
-      open = true;
     };
   };
 
