4b8ebbf68f4d14d7c9a984f4b7c724772bc0a857
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-11-04 00:21:03 +0530

    fix (wip): bufferline error - nixos nixCats nvim
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- nixos/modules/nvim/lua/plugins/theme.lua
+++ nixos/modules/nvim/lua/plugins/theme.lua
@@ -75,7 +75,8 @@ return {
                 optional = true,
                 opts = function(_, opts)
                     if (vim.g.colors_name or ""):find("catppuccin") then
-                        opts.highlights = require("catppuccin.special.bufferline").get_theme()
+                        -- opts.highlights = require("catppuccin.special.bufferline").get_theme()
+                        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
                     end
                 end,
             },
@@ -87,7 +88,8 @@ return {
         optional = true,
         opts = function(_, opts)
             if (vim.g.colors_name or ""):find("catppuccin") then
-                opts.highlights = require("catppuccin.special.bufferline").get_theme()
+                -- opts.highlights = require("catppuccin.special.bufferline").get_theme()
+                opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
             end
         end,
     },
