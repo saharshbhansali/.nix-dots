2608f30b70b8cdfbe0456bb1cafdc31cc7a739cc
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-11-05 02:59:25 +0530

    fix (wip): disable all other vim config except nixCats nvim
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- home-manager/modules/hm-nixvim.nix
+++ home-manager/modules/hm-nixvim.nix
@@ -3,51 +3,53 @@
 {
 
   imports = [ 
-    inputs.nixvim.homeModules.nixvim
+    # inputs.nixvim.homeModules.nixvim
+    inputs.nixCatsNvim.homeModules.default
   ];
 
-  programs.nixvim = {
-    enable = true;
-
-    clipboard.register = "unnamedplus";
-    clipboard.providers.wl-copy.enable = true;
-    clipboard.providers.xclip.enable = true;
-
-    colorschemes.catppuccin.enable = true;
-
-    viAlias = true;
-    vimAlias = true;
-
-    luaLoader.enable = true;
-
-    # plugins = {
-    #   lazy.enable = true;
-    #
-    #   lsp = {
-    #     enable = true;
-    #     servers = {
-    #       lua_ls.enable = true;
-    #       rust_analyzer = {
-    #         enable = true;
-    #         installCargo = true;
-    #         installRustc = true;
-    #       };
-    #     };
-    #   };
-    #
-    #   treesitter.enable = true;
-    #   which-key.enable = true;
-    #   lualine.enable = true;
-    #   yanky.enable = true;
-    #   harpoon.enable = true;
-    #   telescope.enable = true;
-    #   mini.enable = true;
-    #   web-devicons.enable = true;
-    # };
-  };
+  # programs.nixvim = {
+  #   enable = true;
+  #
+  #   clipboard.register = "unnamedplus";
+  #   clipboard.providers.wl-copy.enable = true;
+  #   clipboard.providers.xclip.enable = true;
+  #
+  #   colorschemes.catppuccin.enable = true;
+  #
+  #   viAlias = true;
+  #   vimAlias = true;
+  #
+  #   luaLoader.enable = true;
+  #
+  #   # plugins = {
+  #   #   lazy.enable = true;
+  #   #
+  #   #   lsp = {
+  #   #     enable = true;
+  #   #     servers = {
+  #   #       lua_ls.enable = true;
+  #   #       rust_analyzer = {
+  #   #         enable = true;
+  #   #         installCargo = true;
+  #   #         installRustc = true;
+  #   #       };
+  #   #     };
+  #   #   };
+  #   #
+  #   #   treesitter.enable = true;
+  #   #   which-key.enable = true;
+  #   #   lualine.enable = true;
+  #   #   yanky.enable = true;
+  #   #   harpoon.enable = true;
+  #   #   telescope.enable = true;
+  #   #   mini.enable = true;
+  #   #   web-devicons.enable = true;
+  #   # };
+  # };
 
   ## User level packages
   home.packages = with pkgs; [
+    inputs.nixCatsNvim.packages.${pkgs.system}.nvim
     inputs.nixCatsNvim.packages.${pkgs.system}.testnvim
     vimPlugins.supermaven-nvim
     # vimPlugins.mason-tool-installer-nvim
@@ -63,24 +65,24 @@
   #   recursive = true;
   #   # mutable = true;
   # };
-  home.activation.copyTestNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    set -eu
-  
-    target="$HOME/.config/testnvim"
-    src=${../../configs/nvim}
-  
-    mkdir -p "$HOME/.config"
-  
-    if [ -e "$target" ]; then
-      echo "Found existing $target — backing up before copying..."
-      backup="/tmp/testnvim.backup-$(date +%Y%m%d-%H%M%S)"
-      mv "$target" "$backup"
-      echo "Backed up to $backup"
-    fi
-  
-    cp -r "$src" "$target"
-    chmod -R ug+w "$target"
-    echo "Copied Neovim config to $target"
-  '';
+  # home.activation.copyTestNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #   set -eu
+  #
+  #   target="$HOME/.config/testnvim"
+  #   src=${../../configs/nvim}
+  #
+  #   mkdir -p "$HOME/.config"
+  #
+  #   if [ -e "$target" ]; then
+  #     echo "Found existing $target — backing up before copying..."
+  #     backup="/tmp/testnvim.backup-$(date +%Y%m%d-%H%M%S)"
+  #     mv "$target" "$backup"
+  #     echo "Backed up to $backup"
+  #   fi
+  #
+  #   cp -r "$src" "$target"
+  #   chmod -R ug+w "$target"
+  #   echo "Copied Neovim config to $target"
+  # '';
 
 }
