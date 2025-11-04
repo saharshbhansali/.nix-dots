b802e69fd4f2fea793a6847414cef6025b48a3e0
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-11-05 00:03:46 +0530

    fix (wip): nvim - add treesitter plugins and config for noice, trouble, lualine and treesitter
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

---
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,58 @@
+return {
+    -- Noice plugin
+    {
+        'folke/noice.nvim',
+        config = function()
+            require('noice').setup {
+                lsp = {
+                    override = {
+                        -- Optionally, override LSP progress message display
+                        ['vim.lsp.util.get_progress_messages'] = true,
+                    },
+                },
+                -- You can also add other customizations based on your needs
+            }
+        end,
+    },
+
+    -- Trouble plugin
+    {
+        'folke/trouble.nvim',
+        config = function()
+            require('trouble').setup {}
+        end,
+    },
+
+    -- lualine plugin
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'folke/noice.nvim' }, -- Ensure noice is available when lualine is set up
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = 'gruvbox', -- Adjust according to your color theme
+                },
+                sections = {
+                    lualine_c = { 'filename', require('noice').api.statusline }, -- Integrating Noice with Lualine
+                },
+            }
+        end,
+    },
+
+    -- Treesitter plugin
+    {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate', -- Ensure parsers are updated
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = 'maintained', -- Install only maintained parsers
+                highlight = {
+                    enable = true, -- Enable syntax highlighting
+                },
+                indent = {
+                    enable = true, -- Enable tree-sitter based indentation
+                },
+            }
+        end,
+    },
+}
