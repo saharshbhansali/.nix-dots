90f76c1f67c885624950872a55e10b00f8f5baad
saharshbhansali <saharsh.bhansali15@gmail.com>
2025-11-06 20:25:13 +0530

    refactor: reorganize home manager neovim module
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- home-manager/modules/hm-nixvim.nix
+++ home-manager/modules/hm-nixvim.nix
@@ -7,6 +7,50 @@
     inputs.nixCatsNvim.homeModules.default
   ];
 
+  ## User level packages
+  home.packages = with pkgs; [
+    inputs.nixCatsNvim.packages.${pkgs.system}.nvim
+    inputs.nixCatsNvim.packages.${pkgs.system}.testnvim
+    # vimPlugins.mason-tool-installer-nvim
+    # vimPlugins.mason-nvim-dap-nvim
+    # vimPlugins.mason-nvim
+    # vimPlugins.mason-null-ls-nvim
+    # vimPlugins.mason-lspconfig-nvim
+  ];
+
+  # Config files for nixCatsNvim
+  home.file.".config/nvim" = {
+    source = ../../configs/nvim;
+    recursive = true;
+  };
+
+  # # Test config for experimentation
+  # home.file.".config/testnvim" = {
+  #   source = ../../configs/nvim;
+  #   recursive = true;
+  #   # mutable = true;
+  # };
+  # home.activation.copyTestNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #   set -eu
+  #
+  #   target="$HOME/.config/testnvim"
+  #   src=${../../configs/nvim}
+  #
+  #   mkdir -p "$HOME/.config"
+  #
+  #   if [ -e "$target" ]; then
+  #     echo "Found existing $target — backing up before copying..."
+  #     backup="/tmp/testnvim.backup-$(date +%Y%m%d-%H%M%S)"
+  #     mv "$target" "$backup"
+  #     echo "Backed up to $backup"
+  #   fi
+  #
+  #   cp -r "$src" "$target"
+  #   chmod -R ug+w "$target"
+  #   echo "Copied Neovim config to $target"
+  # '';
+
+  # ## NixVim
   # programs.nixvim = {
   #   enable = true;
   #
@@ -47,47 +91,14 @@
   #   # };
   # };
 
-  ## User level packages
-  home.packages = with pkgs; [
-    inputs.nixCatsNvim.packages.${pkgs.system}.nvim
-    inputs.nixCatsNvim.packages.${pkgs.system}.testnvim
-    vimPlugins.supermaven-nvim
-    # vimPlugins.mason-tool-installer-nvim
-    # vimPlugins.mason-nvim-dap-nvim
-    # vimPlugins.mason-nvim
-    # vimPlugins.mason-null-ls-nvim
-    # vimPlugins.mason-lspconfig-nvim
-  ];
-
-  home.file.".config/nvim" = {
-    source = ../../configs/nvim;
-    recursive = true;
-  };
-
-  # # Test config for experimentation
-  # home.file.".config/testnvim" = {
-  #   source = ../../configs/nvim;
-  #   recursive = true;
-  #   # mutable = true;
-  # };
-  # home.activation.copyTestNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
-  #   set -eu
-  #
-  #   target="$HOME/.config/testnvim"
-  #   src=${../../configs/nvim}
-  #
-  #   mkdir -p "$HOME/.config"
-  #
-  #   if [ -e "$target" ]; then
-  #     echo "Found existing $target — backing up before copying..."
-  #     backup="/tmp/testnvim.backup-$(date +%Y%m%d-%H%M%S)"
-  #     mv "$target" "$backup"
-  #     echo "Backed up to $backup"
-  #   fi
-  #
-  #   cp -r "$src" "$target"
-  #   chmod -R ug+w "$target"
-  #   echo "Copied Neovim config to $target"
-  # '';
+  # ## User level packages
+  # home.packages = with pkgs; [
+  #   vimPlugins.supermaven-nvim
+  #   vimPlugins.mason-tool-installer-nvim
+  #   vimPlugins.mason-nvim-dap-nvim
+  #   vimPlugins.mason-nvim
+  #   vimPlugins.mason-null-ls-nvim
+  #   vimPlugins.mason-lspconfig-nvim
+  # ];
 
 }
