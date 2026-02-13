a83d1137b311e95465f3e5ae7469f2d25acc8f0d
saharshbhansali <saharsh.bhansali@gmail.com>
2026-02-13 23:11:42 +0530

    fix (wip): add (commented out) production nvidia package

--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -43,6 +43,7 @@
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = config.boot.kernelPackages.nvidiaPackages.stable;
+      # package = config.boot.kernelPackages.nvidiaPackages.production;
 
       prime = {
         offload = {
