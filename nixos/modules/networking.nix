b7c57053da7b5d981a4a860938d67278d6340996
saharshbhansali <saharsh.bhansali@gmail.com>
2026-04-18 15:06:31 +0530

    fix: disable powersave mode via modprobe param

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -13,6 +13,7 @@ in
   # boot.kernelParams = [ "pcie_aspm=off" ];
   boot.extraModprobeConfig = ''
     options rtw89_pci disable_clkreq=1 disable_aspm_l1=1 disable_aspm_l1ss=1
+    options rtw89_core disable_ps_mode=1
   '';
 
   # Networking services
