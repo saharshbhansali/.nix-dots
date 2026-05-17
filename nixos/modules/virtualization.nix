710282ca0a5a9d6879f513b63ae26772e33adf60
saharshbhansali <saharsh.bhansali@gmail.com>
2026-05-17 15:22:51 +0530

    feat(virtualization): add podman to configuration

--- nixos/modules/virtualization.nix
+++ nixos/modules/virtualization.nix
@@ -1,10 +1,22 @@
 { config, lib, pkgs, inputs, ... }:
 {
+  # Add user to groups for containerization
+  users.users.saharsh.extraGroups = [ "podman" ];
 
   # Enable containerization
   virtualisation.containers.enable = true;
 
   virtualisation = {
+    podman = {
+      enable = true;
+
+      # # Create a `docker` alias for podman, to use it as a drop-in replacement
+      # dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+
     docker = {
       enable = true;
       storageDriver = "btrfs";
@@ -19,4 +31,13 @@
   systemd.services.docker.wantedBy = lib.mkForce [ ]; # Don't auto-start
   # Manual activation: sudo systemctl start docker
 
+  # Useful other development tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    docker-compose # docker - start group of containers for dev
+    podman-compose # podman - start group of containers for dev
+    podman-tui # status of containers in the terminal
+    podman-desktop # graphical tool for developing on containers and Kubernetes
+    pods # podman desktop application
+  ];
 }
